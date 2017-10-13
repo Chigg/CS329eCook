b95f57783d034a67fde5515d1b060b16da4e2699
Chigg <higgins.colin@gmail.com>
2017-10-13 18:24:38 -0500

    restructuring code to make it easier to add states
    any time you add an image or asset, do it in load.js in order to maintain consistency. This will let us create new maps or states without having to reload assets.

---
+++ main.js
@@ -0,0 +1,8 @@
+var game = new Phaser.Game(600, 400, Phaser.CANVAS);
+
+game.state.add('load', demo.load);
+game.state.add('menu', demo.menu);
+game.state.add('state2', demo.state2);
+game.state.add('gameover', demo.gameover);
+
+game.state.start('load');
