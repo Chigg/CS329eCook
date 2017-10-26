c400f2ee80b242d00a1de46152a27f39b14c8819
Chigg <higgins.colin@gmail.com>
2017-10-25 19:12:52 -0500

    added functions to main, fixed state3 crash. refer to trello.

--- js/main.js
+++ js/main.js
@@ -29,10 +29,292 @@ function collectHP (player, HPDrop) {
     }
 }
 
-function goToMedCenter () {
-    game.state.start ('state3');
+
+function collisionHandler (bullet, baddie) {
+
+    //  When a bullet hits an carrot we kill them both
+    bullet.kill();
+    baddie.kill();
+    particleBurst(baddie, baddie.x,baddie.y);
+    score += 10;
+    scoreText.text = 'Score: ' + score;
 }
 
-function goToGarden () {
-    game.state.start ('state2');
+function explosionCollisionHandler (explosion, baddie) {
+    
+    //  When a bullet hits an carrot we kill them both
+    baddie.kill();
+    particleBurst(baddie.x,baddie.y);
+    score += 10;
+    scoreText.text = 'Score: ' + score;
 }
+
+//this should be temporary, i don't think it's best practice
+function ARcollisionHandler (carrotAmmo, baddie) {
+
+    //  When a bullet hits a carrot we kill them both
+    carrotAmmo.kill();
+    baddie.kill();
+    particleBurst(baddie.x,baddie.y);
+    score += 10;
+    scoreText.text = 'Score: ' + score;
+}
+
+function GcollisionHandler (grenade, baddie) {
+
+    //  When a bullet hits an carrot we kill them both
+    grenade.kill();
+    baddie.kill();
+    particleBurst(baddie.x,baddie.y);
+    explode(baddie.x,baddie.y);
+    score += 10;
+    scoreText.text = 'Score: ' + score;
+}
+
+function throwKnife(){
+    
+    if(game.time.now > nextFire && bullets.countDead() > 0)
+    {
+        nextFire = game.time.now + fireRate;
+        
+        var bullet = bullets.getFirstDead();
+        
+        //initial firing position. Right now it is centered on player.
+        bullet.reset(player.x + 35, player.y + 30);
+        bullet.anchor.setTo(0.5,0.5);
+        bullet.rotation = game.physics.arcade.angleToPointer(bullet);
+        game.physics.arcade.moveToPointer(bullet, 800);
+        this.checkWorldBounds = true;
+        this.outOfBoundsKill = true; 
+        ammo1 -= 1;
+        
+        if (look_left){
+                player.animations.play('meleeLeft');
+                meleeSound.play()
+                }
+        else {
+                player.animations.play('meleeRight');
+                meleeSound.play()
+             }
+    }
+}
+
+function ARFire(){
+    //for the AR
+    if(game.time.now > Wep2nextFire && AR.countDead() > 0)
+    {
+        Wep2nextFire = game.time.now + Wep2fireRate;
+        
+        var carrotAmmo = AR.getFirstDead();
+        
+        //initial firing position. Right now it is centered on player.
+        carrotAmmo.reset(player.x + 30, player.y + 30);
+        carrotAmmo.anchor.setTo(0.5,0.5);
+        carrotAmmo.rotation = game.physics.arcade.angleToPointer(carrotAmmo);
+        game.physics.arcade.moveToPointer(carrotAmmo, 1500);
+        carrotAmmo.lifespan = 300;
+        this.checkWorldBounds = true;
+        this.outOfBoundsKill = true;
+        ammo2 -= 1;
+        
+        if (look_left){
+                player.animations.play('meleeLeft');
+                ar_sound.play()
+                }
+        else {
+                player.animations.play('meleeRight');
+                ar_sound.play()
+             }
+    }
+}
+
+function throwGrenade(){
+    
+    if(game.time.now > nextFire && grenades.countDead() > 0)
+    {
+        nextFire = game.time.now + GfireRate;
+        
+        var grenade = grenades.getFirstDead();
+        
+        //initial firing position. Right now it is centered on player.
+        grenade.reset(player.x + 35, player.y + 30);
+        grenade.anchor.setTo(0.5,0.5);
+        game.physics.arcade.moveToPointer(grenade, 350);
+
+        ammo3 -= 1;
+        if (look_left){
+                player.animations.play('meleeLeft');
+                meleeSound.play()
+                }
+        else {
+                player.animations.play('meleeRight');
+                meleeSound.play()
+             }
+    }
+}
+
+function explode (x, y) {
+    
+    var explosion = explosions.getFirstDead();
+        
+        //initial firing position. Right now it is centered on player.
+    explosion.x = x
+    explosion.y = y
+    
+    explosion.animations.add('explode', [0,1,2,3,4,5,6,7,8,9], 0, true);
+    explosion.animations.play('explode');
+    explosion_sound.play()
+    explosion.animations.getAnimation('explode').delay = 120;
+    
+    explosion.reset(explosion.x, explosion.y);
+    explosion.play()
+    //controls how long the explosion exists
+    explosion.lifespan = 2000;
+    
+}
+
+
+// player loses health when hit by enemy
+function loseHealth (player, baddies) {
+    playerHP -= 1;
+    HPFrame += 1;
+    grunt.play()
+    health_bar.frame = HPFrame;
+    HPText.text = 'Health: ' + playerHP;
+    
+}
+
+function move (baddie) {
+    if (baddie.body.velocity == 0) {
+           baddie.animations.play ("bRight");
+    }
+    
+   if (game.physics.arcade.distanceBetween(baddie, player) <= attackDistance) {
+       game.physics.arcade.moveToObject(baddie,player,200);
+       
+       
+       // moving left
+       if (baddie.body.velocity.x < 0) {
+           baddie.animations.play("bLeft");
+           
+       // moving right   
+       } else if (baddie.body.velocity.x > 0) {
+           baddie.animations.play("bRight");
+           
+       }
+    }
+}
+
+function b_move (broccoli) {
+    if (broccoli.body.velocity == 0) {
+           broccoli.animations.play ("broRight");
+    }
+    
+   if (game.physics.arcade.distanceBetween(broccoli, player) <= attackDistance) {
+       game.physics.arcade.moveToObject(broccoli,player,100);
+       
+       
+       // moving left
+       if (broccoli.body.velocity.x < 0) {
+           broccoli.animations.play("broLeft");
+           
+       // moving right   
+       } else if (broccoli.body.velocity.x > 0) {
+           broccoli.animations.play("broRight");
+           
+       }
+    }
+}
+
+function resetGame() {
+    game.state.start('gameover');
+    
+}
+
+function meleeLeft(){
+    player.animations.play('meleeLeft');
+    meleeSound.play()
+}
+
+function meleeRight(){
+    player.animations.play('meleeRight')
+    meleeSound.play()
+}
+
+function spawnEnemy() {
+    
+    for (var i = 0; i < Math.random(0,100); i++)
+            {
+                var baddie = baddies.create(game.world.randomX, game.world.randomY, 'baddie');
+                //baddie animations
+                baddie.animations.add('bRight',[5,6,7], 16, true);
+                baddie.animations.add('bLeft',[8,9,10], 16, true);
+                baddie.animations.add('meleeRight', [0,1,2], true);
+                baddie.animations.add('meleeLeft', [13,14,15], true);
+                
+                
+                baddie.animations.play("bRight");
+            }
+    
+    for (var i = 0; i < Math.random(0,100); i++)
+            {
+                var broccoli = broccolis.create(game.world.randomX, game.world.randomY, 'broccoli');
+//                var broccoli = broccolis.create(game.world.randomX, game.world.randomY, 'broccoli');
+                //broccoli animations
+                broccoli.animations.add('broRight', [4,5,6,7], 0, true);
+                broccoli.animations.add('broLeft', [0,1,2,3], 0, true);
+                
+                
+                broccoli.animations.play("broRight");
+            }
+    
+    
+    
+    spawning = false;
+    
+}
+
+function collectAmmo (player, ammo) {
+
+    // Removes the ammo from the screen
+    ammo.kill();
+    
+    if(knifeOut){
+        ammo1 += 20;
+    }
+        
+    if(wep2Out){
+        ammo2 += 10;
+    }   
+    
+    if(wep3Out){
+    }
+
+
+}
+
+ function particleBurst(x, y) {
+
+     //  Position the emitter where the mouse/touch event was
+     emitter.x = x;
+     emitter.y = y;
+
+     //  The first parameter sets the effect to "explode" which means all particles are emitted at once
+     //  The second gives each particle a 2000ms lifespan
+     //  The third is ignored when using burst/explode mode
+     //  The final parameter (10) is how many particles will be emitted in this single burst
+     emitter.start(true, 500, null, 5);
+     var randSound = Math.random(0,3);
+     
+     if(randSound = 1){
+         smash.play()
+     }
+     if(randSound = 2){
+         whack.play()
+     }
+     if(randSound = 3){
+         splat.play()
+     }
+     
+
+ }
--- js/state3.js
+++ js/state3.js
@@ -17,74 +17,85 @@ demo.state3.prototype = {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.stage.backgroundColor = '#008000';
 
+        //spacebar is for melee but it's not implemented currently    
         
-        //spacebar is for melee but it's not implemented currently
         attackButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        
+
         //weapon selection goes here
         wep1 = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
         wep2 = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
         wep3 = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
         baddies = game.add.physicsGroup(Phaser.Physics.ARCADE);
+        broccolis = game.add.physicsGroup(Phaser.Physics.ARCADE);
+        explosions = game.add.physicsGroup(Phaser.Physics.ARCADE);
         
-        emitter = game.add.emitter(0, 0, 100);
+        emitter = game.add.emitter(0, 0, 50);
 
         emitter.makeParticles('chunk');
-        emitter.gravity = 0;
-       
+        emitter.gravity = -10;
+
         player = game.add.sprite(game.world.centerX, game.world.height - 200, 'player');
         player.enableBody = true;
         player.physicsBodyType = Phaser.Physics.ARCADE;
-        
+
         //animations  
         //player animations
         player.animations.add('right', [0,1,2,3,4,5,6], 13, true);
         player.animations.add('left', [7,8,9,10,11,12,13], 13, true);
-        
+
         player.frame = 0;
         player.animations.add('meleeRight', [14,15,16], 0, false);
         player.animations.add('meleeLeft', [17,18,19], 0, false);
-
         
+
         //audio
         meleeSound = game.add.audio('melee_sound');
         ar_sound = game.add.audio('ar_sound');
+        explosion_sound = game.add.audio('explosion');
         smash = game.add.audio('smash');
+        grunt = game.add.audio('grunt');
         whack = game.add.audio('whack');
         splat = game.add.audio('splat');
-        
+
         //physics
         game.physics.arcade.enable(player);
-        
+
         game.camera.follow(player);
-        
+
         player.body.bounce.y = 0;
         player.body.gravity.y = 0;
         player.body.collideWorldBounds = true;
-        
+
         // enemy spawns and behavior
         baddies = game.add.group();
         baddies.enableBody = true;
         
+        broccolis = game.add.group();
+        broccolis.enableBody = true;
+
         xCoord = Math.random(0, 1920);
         yCoord = Math.random(0, 1920);
-        
-//        for (var i = 0; i < 15; i++)
-//            {
-//                var baddie = baddies.create(game.world.randomX, game.world.randomY, 'baddie');
-//                //baddie animations
-//                baddie.animations.add('bRight',[5,6,7], 16, true);
-//                baddie.animations.add('bLeft',[8,9,10], 16, true);
-//                baddie.animations.add('meleeRight', [0,1,2], true);
-//                baddie.animations.add('meleeLeft', [13,14,15], true);
-//                
-//                baddie.animations.play("bRight");
-//            }
-        
+
+        med_center = game.add.sprite(150, 150, 'med_center');
+        game.physics.arcade.enable(med_center);
+        med_center.enableBody = true;
+
+    //        for (var i = 0; i < 15; i++)
+    //            {
+    //                var baddie = baddies.create(game.world.randomX, game.world.randomY, 'baddie');
+    //                //baddie animations
+    //                baddie.animations.add('bRight',[5,6,7], 16, true);
+    //                baddie.animations.add('bLeft',[8,9,10], 16, true);
+    //                baddie.animations.add('meleeRight', [0,1,2], true);
+    //                baddie.animations.add('meleeLeft', [13,14,15], true);
+    //                
+    //                baddie.animations.play("bRight");
+    //            }
+
     //this is where we establish ammo refilling
         ammos = game.add.group();
         ammos.enableBody = true;
-        
+
         HPDrops = game.add.group();
         HPDrops.enableBody = true;
 
@@ -96,6 +107,7 @@ demo.state3.prototype = {
             var HPDrop = HPDrops.create(game.world.randomX, game.world.randomY, 'extra_life');
 
         }
+
     //this is where we establish knife projectiles
         bullets = game.add.group();
         bullets.enableBody = true;
@@ -104,9 +116,15 @@ demo.state3.prototype = {
         bullets.setAll('checkWorldBounds', true);
         bullets.setAll('outOfBoundsKill', true);
 
+    //explosions
+        explosions = game.add.group();
+        explosions.enableBody = true;
+        explosions.physicsBodyType = Phaser.Physics.ARCADE;
+        explosions.createMultiple(50, 'explosion');
         
+
     //this is where we establish AR projectiles
-        
+
         AR = game.add.group();
         AR.enableBody = true;
         AR.physicsBodyType = Phaser.Physics.ARCADE;
@@ -114,40 +132,72 @@ demo.state3.prototype = {
         AR.setAll('checkWorldBounds', true);
         AR.setAll('outOfBoundsKill', true);
         
+    //this is where we establish AR projectiles
+
+        grenades = game.add.group();
+        grenades.enableBody = true;
+        grenades.physicsBodyType = Phaser.Physics.ARCADE;
+        grenades.createMultiple(100, 'grenade');
+        grenades.setAll('checkWorldBounds', true);
+        grenades.setAll('outOfBoundsKill', true);
+
     //this is where we establish shotgun projectiles
-//        shells = game.add.group();
-//        shells.enableBody = true;
-        
+    //        shells = game.add.group();
+    //        shells.enableBody = true;
+
         xCoord = Math.random(0, 1920);
         yCoord = Math.random(0, 1920);
-        
-        
+
         HPText = game.add.text(16, 16, 'Health: ' + playerHP, {fontSize: '15px', fill: '#000'});
         scoreText = game.add.text(16, 16, 'Score: ' + score, {fontSize: '15px', fill: '#000'});    
         ammoText = game.add.text(16, 16, 'Ammo: ' + '∞', {fontSize: '15px', fill: '#000'});
+        weaponsText = game.add.text(16, 16, '1    2    3', {fontSize: '15px', fill: '#000'});
         //difficultyText = game.add.text(16, 16, 'difficulty: ' + difficulty, {fontSize: '15px', fill: '#000'});  
         
         GardenText = game.add.text(game.world.centerX, game.world.height - 150, 'Go To Garden', {fontSize:'20px', fill: '#000'});
-        
         succulent = game.add.sprite(game.world.centerX, game.world.height - 100, 'succulent');
         game.physics.enable(succulent);
         
         health_bar = game.add.sprite(16, 16, 'health_bar');
         
+        //0 UnSelected, 1 Selected, 2 No Ammo, 3 No Ammo, 4Locked
+        //knife
+        k_ui = game.add.sprite(0,10, 'knife_ui');
+        k_ui.animations.add('us', [0], 0, true);
+        k_ui.animations.add('s', [1], 1, true);
+        k_ui.animations.add('na', [2], 2, true);
+        k_ui.animations.add('nas', [3], 3, true);
+        k_ui.animations.add('l', [4], 4, true);
+        //Assult Rifle
+        ar_ui = game.add.sprite(0,10, 'ar_ui');
+        ar_ui.animations.add('us', [0], 0, true);
+        ar_ui.animations.add('s', [1], 1, true);
+        ar_ui.animations.add('na', [2], 2, true);
+        ar_ui.animations.add('nas', [3], 3, true);
+        ar_ui.animations.add('l', [4], 4, true);
+        //Grenade
+        g_ui = game.add.sprite(0,10, 'grenade_ui');
+        g_ui.animations.add('us', [0], 0, true);
+        g_ui.animations.add('s', [1], 1, true);
+        g_ui.animations.add('na', [2], 2, true);
+        g_ui.animations.add('nas', [3], 3, true);
+        g_ui.animations.add('l', [4], 4, true);
+
         w=game.input.keyboard.addKey(Phaser.Keyboard.W);
         a=game.input.keyboard.addKey(Phaser.Keyboard.A);
         s=game.input.keyboard.addKey(Phaser.Keyboard.S);
         d=game.input.keyboard.addKey(Phaser.Keyboard.D);
         spawning = true;
-        
-        
+
+
         crosshair = game.add.sprite(game.world.centerX, game.world.centerY, 'crosshair');
         crosshair.fixedToCamera = true;
         crosshair.cameraOffset.setTo(0,0);
-        
+
         game.physics.arcade.enable(crosshair);
-            
+
         cursors = game.input.keyboard.createCursorKeys();
+        
     },
     
     
@@ -168,15 +218,28 @@ demo.state3.prototype = {
         ammoText.fixedToCamera = true;
         ammoText.cameraOffset.setTo(490, 370);
         
+        weaponsText.fixedToCamera = true;
+        weaponsText.cameraOffset.setTo(10, 350);
+        
+        k_ui.fixedToCamera = true;
+        k_ui.cameraOffset.setTo(5, 370);
+        
+        ar_ui.fixedToCamera = true;
+        ar_ui.cameraOffset.setTo(30, 370);
+        
+        g_ui.fixedToCamera = true;
+        g_ui.cameraOffset.setTo(55, 370);
+        
         game.physics.arcade.overlap(player, ammos, collectAmmo, null, this);
         game.physics.arcade.overlap(player, HPDrops, collectHP, null, this);
         
-        game.physics.arcade.overlap(player, succulent, goToGarden);
+        game.physics.arcade.collide(player, 'blockedLayer');
         
-        //for debugging difficulty
-        //difficultyText.fixedToCamera = true;
-        //difficultyText.cameraOffset.setTo(135, 60);
-       
+        
+        game.physics.arcade.overlap(player, med_center, goToMedCenter);
+        
+        console.log(game.physics.arcade.overlap(player, med_center));
+        
         /*//if the distance to pointer is greater than 50, the sprite while move to the new cursor position
         if(game.physics.arcade.distanceToPointer(crosshair, game.input.activePointer)> 50)
             {
@@ -195,10 +258,12 @@ demo.state3.prototype = {
         crosshair.cameraOffset.setTo((game.input.mousePointer.x)-25, (game.input.mousePointer.y)-25);
         
         baddies.forEach(move);
+        broccolis.forEach(b_move);
         
         cursors = game.input.keyboard.createCursorKeys();
 
         game.physics.arcade.overlap(player, baddies, loseHealth, null, this);
+        game.physics.arcade.overlap(player, broccolis, loseHealth, null, this);
         
         
         if (w.isDown || a.isDown || s.isDown || d.isDown)
@@ -276,7 +341,74 @@ demo.state3.prototype = {
             knifeOut = false;
             wep2Out = false;
             wep3Out = true;
+        }
+        
+        if (knifeOut){
+            ammoText.text = 'Knives: ' + ammo1;
+            if (ammo1 == 0){
+            k_ui.animations.play('nas');
+            }
+            else{
+                k_ui.animations.play('s');
+            }
+            if (ammo2 == 0){
+                ar_ui.animations.play('na');
+            }
+            else{
+                ar_ui.animations.play('us');
+            }
             
+            if (ammo3 == 0){
+                g_ui.animations.play('na');
+            }
+            else{
+                g_ui.animations.play('us');
+            }
+            
+        }
+        if(wep2Out){
+            ammoText.text = 'AR Ammo: ' + ammo2;
+            if (ammo1 == 0){
+                k_ui.animations.play('na');
+            }
+            else{
+                k_ui.animations.play('us');
+            }
+            if (ammo2 == 0){
+                ar_ui.animations.play('nas');
+            }
+            else{
+                ar_ui.animations.play('s');
+            }
+            
+            if (ammo3 == 0){
+                g_ui.animations.play('na');
+            }
+            else{
+                g_ui.animations.play('us');
+            }
+        }
+        if(wep3Out){
+            ammoText.text = 'Grenades: ' + ammo3;
+            if (ammo1 == 0){
+                k_ui.animations.play('na');
+            }
+            else{
+                k_ui.animations.play('us');
+            }
+            if (ammo2 == 0){
+                ar_ui.animations.play('na');
+            }
+            else{
+                ar_ui.animations.play('us');
+            }
+            
+            if (ammo3 == 0){
+                g_ui.animations.play('nas');
+            }
+            else{
+                g_ui.animations.play('s');
+            }
         }
         
         if (game.input.activePointer.isDown)
@@ -299,7 +431,18 @@ demo.state3.prototype = {
         
     game.physics.arcade.overlap(bullets, baddies, collisionHandler, null, this);
     game.physics.arcade.overlap(AR, baddies, ARcollisionHandler, null, this);
-
+    game.physics.arcade.overlap(grenades, baddies, GcollisionHandler, null, this);
+    game.physics.arcade.overlap(explosions, baddies, explosionCollisionHandler, null, this);
+        
+    game.physics.arcade.overlap(bullets, broccolis, collisionHandler, null, this);
+    game.physics.arcade.overlap(AR, broccolis, ARcollisionHandler, null, this);
+    game.physics.arcade.overlap(grenades, broccolis, GcollisionHandler, null, this);
+    game.physics.arcade.overlap(explosions, broccolis, explosionCollisionHandler, null, this);
+    game.physics.arcade.overlap(player, succulent, goToGarden);
 
     }
 };
+
+function goToGarden () {
+    game.state.start ('state2');
+}
